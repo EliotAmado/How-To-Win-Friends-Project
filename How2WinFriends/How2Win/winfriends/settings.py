9fdea0107af3cda783f9ac2e72346d66e47102ab
EliotAmado <amadoeliot14@gmail.com>
2022-12-12 19:22:43 -0600

    Add database

--- How2WinFriends/How2Win/winfriends/settings.py
+++ How2WinFriends/How2Win/winfriends/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import dj_database_url
 from dotenv import load_dotenv
 import os
 from pathlib import Path
@@ -76,13 +77,21 @@ WSGI_APPLICATION = 'winfriends.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
